856f5726c14c4aadcb1079daf58a40809338ec38
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2024-12-11 12:54:23 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ThreeScene from "./components/ThreeScene";
+
+const App = () => {
+  return (
+    <div style={{ margin: 0, padding: 0, overflow: "hidden" }}>
+      <ThreeScene />
+    </div>
+  );
+};
+
+export default App;
